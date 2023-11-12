d20055f677f3e6084e6172b438c93d9ad632e966
FreddyPinto <freddypinto@outlook.com>
2023-11-11 22:23:08 -0500

    Add EventItem component

---
+++ src/components/Events/components/EventItem/index.jsx
@@ -0,0 +1,12 @@
+const EventItem = ({ info, id, name, image, onEventClick }) => {
+    return (
+        <div onClick={() => onEventClick(id)}>
+            <img src={image} alt={name} width={200} height={200} />
+            <h4>{name}</h4>
+            <p>{info}</p>
+            <button onClick={() => onEventClick}>Ver mas</button>
+        </div>
+    );
+};
+
+export default EventItem;
