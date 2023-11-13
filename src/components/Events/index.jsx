d0be5020749e079a8d0ff4247024365774b79786
FreddyPinto <freddypinto@outlook.com>
2023-11-12 23:49:22 -0500

    Add events search

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import EventItem from "./components/EventItem";
 import eventsJSON from "../../data/events.json";
 
-const Events = () => {
+const Events = ({ searchTerm }) => {
     const [data] = useState(eventsJSON);
     const { _embedded: { events } } = data;
 
@@ -10,19 +10,31 @@ const Events = () => {
         console.log('Evento clickeado: ',id);
     }
 
+    const renderEvents = () => {
+        let eventsFiltered = events;
+
+        if (searchTerm.length > 0) {
+            eventsFiltered = eventsFiltered.filter((event) => event.name.toLocaleLowerCase().includes(searchTerm.toLowerCase()));
+        }
+
+        return eventsFiltered.map((event) => {
+            return (
+                <EventItem
+                    key={event.id}
+                    info={event.info}
+                    id={event.id}
+                    name={event.name}
+                    image={event.images[0].url}
+                    onEventClick={handleEventItemClick}
+                />
+            );
+        });
+    };
+
     return (
         <div>
             Eventos
-            {events.map((event) => (
-                <EventItem
-                    key={event.id}
-                    name={event.name}
-                    info={event.info}
-                    image={event.images[0].url}
-                    onEventClick={handleEventItemClick}
-                    id={event.id}
-                />
-            ))}
+            {renderEvents()}
         </div>
     );
 }
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,18 +1,25 @@
 import  {useState} from 'react';
 
-const Navbar = () => {
+const Navbar = ({ onSearch}) => {
     const [search, setSearch] = useState('');
 
     const handleInputchange = (evt) => {
         setSearch(evt.target.value);
     };
 
+const handleInputKeyDown = (evt) => {
+    if (evt.key === 'Enter') {
+        onSearch(search);
+    } 
+};
+
     return (
         <div>
             <p>Mi Boletera</p>
             <input 
                 placeholder="Busca tu evento favorito"
-                onChange={handleInputchange} 
+                onChange={handleInputchange}
+                onKeyDown={handleInputKeyDown} 
                 value={search}
             />
         </div>
