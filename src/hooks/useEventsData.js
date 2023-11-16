9d184f02b51dd7b9e665307c08bd9e10b6549629
FreddyPinto <freddypinto@outlook.com>
2023-11-15 20:58:02 -0500

    Add API request status

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,13 +1,29 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import eventsJSON from '../data/events.json';
 
 const useEventsData = () => {
-    const [data] = useState(eventsJSON);
-    const { _embedded: { events }  } = data;
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
 
-    return { 
-        events 
+    useEffect(() => {
+        setTimeout(() => {
+            try {
+                setData(eventsJSON);
+                setLoading(false);
+            } catch (error) {
+                setError(error);
+            }
+
+        }, 4000);
+        // Load API call
+    }, []);
+
+    return {
+        events: data?._embedded?.events || [],
+        loading,
+        error,
     };
 };
 
