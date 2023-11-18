0198f5cc7d92083970290262e1a56ca48e800b0d
FreddyPinto <freddypinto@outlook.com>
2023-11-17 23:31:06 -0500

    Clean App.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,8 @@
 import Routes from "./routes";
-// import { useState, useRef } from "react";
-
-// import Navbar from "./components/Navbar";
-// import Events from "./components/Events";
 import "./App.css";
 
 function App() {
-  // const [searchTerm, setSearchTerm] = useState("");
-  // const containerRef = useRef();
-
-  // const handleNavbarSearch = (term) => {
-  //   setSearchTerm(term);
-  // };
-
   return <Routes />;
-    <>
-      {/* <Navbar onSearch={handleNavbarSearch} ref={containerRef}/>
-      <Events searchTerm={searchTerm} /> */}
-    </>
-    
 }
 
 export default App;
