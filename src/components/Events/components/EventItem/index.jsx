635607ed0cbc5e43fe178f7ed7ffc4b46978d756
FreddyPinto <freddypinto@outlook.com>
2023-11-18 00:29:23 -0500

    Adds handleSeeMoreClick function

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,6 +1,12 @@
 import styles from './EventItem.module.css';
 
 const EventItem = ({ info, id, name, image, onEventClick }) => {
+    
+    const handleSeeMoreClick = (evt) => {
+        evt.stopPropagation();
+        onEventClick(id);
+    };
+    
     return (
         <div onClick={() => onEventClick(id)} className={styles.eventItemContainer}>
             <div className={styles.imageContainer}>
@@ -9,7 +15,9 @@ const EventItem = ({ info, id, name, image, onEventClick }) => {
             <div className={styles.eventInfoContainer}>
                 <h4 className={styles.eventName}>{name}</h4>
                 <p className={styles.eventInfo}>{info}</p>
-                <button onClick={() => onEventClick} className={styles.seeMoreBtn}>Ver mas</button>
+                <button onClick={handleSeeMoreClick} className={styles.seeMoreBtn}>
+                    Ver mas
+                </button>
             </div>
         </div>
     );
