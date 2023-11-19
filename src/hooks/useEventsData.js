f22d582417b9ebeee803317024396e52a235bb31
FreddyPinto <freddypinto@outlook.com>
2023-11-18 23:38:02 -0500

    Refactor useEventsData to use the Ticketmaster API

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,24 +1,21 @@
-import { useState, useEffect } from 'react';
-
-import eventsJSON from '../data/events.json';
+import { useState } from 'react';
 
 const useEventsData = () => {
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState();
 
-    useEffect(() => {
-        setTimeout(() => {
-            try {
-                setData(eventsJSON);
-                setLoading(false);
-            } catch (error) {
-                setError(error);
-            }
+    const fetchEvents = async (params) => {
+        try {
+            const response = await fetch('https://app.ticketmaster.com/discovery/v2/?apikey=2N9nueA04IyQUWKKKrYBOtwWjmqEpTsp&countryCode=MX');
+            const data = await response.json();
 
-        }, 4000);
-        // Load API call
-    }, []);
+            setData(data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+        }
+    };
 
     return {
         events: data?._embedded?.events || [],
