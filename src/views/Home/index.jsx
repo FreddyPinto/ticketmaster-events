8b9ad67209b79eb495bdde52a8a05aa7d1b2c406
FreddyPinto <freddypinto@outlook.com>
2023-11-21 00:02:44 -0500

    Feature: Added pagination to Home component

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState, useRef } from "react";
+import ReactPaginate from "react-paginate";
 
 import Navbar from "../../components/Navbar";
 import Events from "../../components/Events";
 import useEventsData from "../../hooks/useEventsData";
+import styles from "./Home.module.css";
 
 const Home = () => {
-    const { events, loading, error, fetchEvents } = useEventsData();
+    const { events, loading, error, fetchEvents, page } = useEventsData();
     const [searchTerm, setSearchTerm] = useState("");
     const containerRef = useRef();
 
@@ -18,19 +20,46 @@ const Home = () => {
         fetchEvents(`&keyword=${term}`);
     };
 
-    if (error) {
-        return <div>Error</div>; 
-    }
+    const handlePageClick = ({ selected }) => {
+        console.log(selected);
+        fetchEvents(`&keyword=${searchTerm}&page=${selected}`);
+    };
 
-    if (loading) {
-        return <div>Cargando...</div>; 
-    }
+    const renderEvents = () => {
+        if (loading) {
+            return <div>Cargando...</div>;
+        }
+
+        if (error) {
+            return <div>Error</div>;
+        }
+
+        return (
+            <div>
+                <Events searchTerm={searchTerm} events={events} />;
+                <ReactPaginate
+                    className={styles.pagination}
+                    nextClassName={styles.next}
+                    previousClassName={styles.previous}
+                    pageClassName={styles.page}
+                    activeClassName={styles.activePage}
+                    disabledClassName={styles.disabledPage}
+                    breakLabel="..."
+                    nextLabel=">"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={page.totalPages}
+                    previousLabel="<"
+                    renderOnZeroPageCount={null}
+                />
+            </div>
+        );
+    };
 
     return (
         <>
             <Navbar onSearch={handleNavbarSearch} ref={containerRef} />
-            {loading ? <div>Cargando...</div> : <Events searchTerm={searchTerm} events={events} />}
-            {!!error && <div>Error</div>}
+            {renderEvents()}
         </>
     );
 };
